550e2184786d6ed56c3840aa5ee1d254591c5f35
Git-Abhi-09 <abhisheksomavanshi123@gmail.com>
2025-07-06 20:18:20 +0530

    build apk file and add same demo pages

--- SelfMart/src/bottomScreen/Card.jsx
+++ SelfMart/src/bottomScreen/Card.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const Card = () => {
     return (
-        <View>
-            <Text>Card</Text>
+        <View style={styles.container}>
+            <Image
+                source={require('../images/purchase.png')}
+                style={styles.image}
+                resizeMode="contain"
+            />
+            <Text style={styles.text}>Coming Soon</Text>
         </View>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    image: {
+        width: 150,
+        height: 150,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 22,
+        fontWeight: '600',
+        color: '#555',
+    },
+});
--- SelfMart/src/bottomScreen/Wishlist.jsx
+++ SelfMart/src/bottomScreen/Wishlist.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const Wishlist = () => {
     return (
-        <View>
-            <Text>Wishlist</Text>
+        <View style={styles.container}>
+            <Image
+                source={require('../images/like.png')}
+                style={styles.image}
+                resizeMode="contain"
+            />
+            <Text style={styles.text}>Wishlist Coming Soon</Text>
         </View>
-    )
-}
+    );
+};
 
-export default Wishlist
+export default Wishlist;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    image: {
+        width: 150,
+        height: 150,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 22,
+        fontWeight: '600',
+        color: '#555',
+    },
+});
